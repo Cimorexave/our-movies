ff3683bf3cca57370f3e2e23f63c4f8afe3d25a6
Cimorexave <sadeq.s502@gmail.com>
2023-09-07 01:43:03 +0330

    wrapping in context

--- App.js
+++ App.js
@@ -2,15 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import Footer from './components/Footer';
 import { styles } from './styles/app.styles';
+import { AppContext, initialAppContext } from './store/app.context';
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="inverted" />
+    <AppContext.Provider value={initialAppContext}>
+      <View style={styles.container}>
+        <StatusBar style="inverted" />
 
-      <Footer></Footer>
+        <Footer></Footer>
 
-    </View>
+      </View>
+    </AppContext.Provider>
   );
 }
