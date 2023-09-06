3cb24e1615dfbc074a9846ad83b3ab4eb85c3202
Cimorexave <sadeq.s502@gmail.com>
2023-09-07 01:34:13 +0330

    managing the state of selected page

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,24 @@
 import { View, Text, Pressable } from "react-native";
 import { styles } from "../styles/app.styles";
+import { useState } from "react";
 
 export default function Footer() {
-
+    const [selectedPageName, setSelectedPageName] = useState("pick");
     return (
         <View style={styles.footer}>
             <Pressable
-                style={styles.pressable}
+                style={[styles.pressable, selectedPageName === "pick" ? styles.pressed : {}]}
+                // onPress={() => console.log('jhi')}
+                onPress={() => setSelectedPageName("pick")}
             >
                 <Text style={styles.text}>
                     Pick
                 </Text>
             </Pressable>
-            <Pressable style={styles.pressable}>
+            <Pressable
+                style={[styles.pressable, selectedPageName === "browse" ? styles.pressed : {}]}
+                onPress={() => setSelectedPageName("browse")}
+            >
                 <Text style={styles.text}>
                     Browse
                 </Text>
