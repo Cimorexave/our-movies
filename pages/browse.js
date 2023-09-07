231758713f58808f0c49d26e9d84875b4334bc28
Cimorexave <sadeq.s502@gmail.com>
2023-09-07 11:41:37 +0330

    modal

--- pages/browse.js
+++ pages/browse.js
@@ -1,15 +1,24 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
 import { styles } from "../styles/app.styles";
 import { browseStyles } from "../styles/browse.styles";
 import { useState } from "react";
+import Modal from "react-native-modal";
 
 export default Browse = () => {
     const [isModalVisible, setModalVisible] = useState(false);
     return (
         <View style={styles.page}>
-            <Pressable onPress={() => setModalVisible(!isModalVisible)}>
+            <TouchableOpacity onPress={() => setModalVisible(!isModalVisible)}>
                 <Text style={[styles.text, browseStyles.addButton]}>Add +</Text>
-            </Pressable>
+            </TouchableOpacity>
+            <Modal isVisible={isModalVisible}>
+                <View style={styles.modal}>
+                    <Text>Modal Content Goes Here</Text>
+                    <TouchableOpacity onPress={() => setModalVisible(false)}>
+                        <Text>Close Modal</Text>
+                    </TouchableOpacity>
+                </View>
+            </Modal>
         </View>
     );
 }
