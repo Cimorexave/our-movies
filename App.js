86a2120202bc94477d50a4d660d1a733090ea324
Cimorexave <sadeq.s502@gmail.com>
2023-09-07 13:14:20 +0330

    providing storage context wrapper

--- App.js
+++ App.js
@@ -3,21 +3,24 @@ import { View, Text } from 'react-native';
 import Footer from './components/Footer';
 import { styles } from './styles/app.styles';
 import AppContext from './store/app.context';
-import { useContext, useState } from 'react';
+import StorageContext from './store/storage.context';
+import { useState } from 'react';
 import Pick from './pages/pick';
 import Browse from './pages/browse';
 
 export default function App() {
   const [appContext, setAppContext] = useState({ pageName: "pick", selectedGenres: [] });
   return (
-    <AppContext.Provider value={{ appContext, setAppContext }}>
-      <View style={styles.container}>
-        <StatusBar style="inverted" />
-        {appContext.pageName === "pick" ? <Pick></Pick>
-          : appContext.pageName === "browse" ? <Browse></Browse> : null}
-        <Footer></Footer>
+    <StorageContext.Provider>
+      <AppContext.Provider value={{ appContext, setAppContext }}>
+        <View style={styles.container}>
+          <StatusBar style="inverted" />
+          {appContext.pageName === "pick" ? <Pick></Pick>
+            : appContext.pageName === "browse" ? <Browse></Browse> : null}
+          <Footer></Footer>
 
-      </View>
-    </AppContext.Provider>
+        </View>
+      </AppContext.Provider>
+    </StorageContext.Provider>
   );
 }
