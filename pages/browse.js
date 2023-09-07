466958c87b88cd2293fc1506ec878acc21cd985b
Cimorexave <sadeq.s502@gmail.com>
2023-09-07 11:04:39 +0330

    setting up pages

---
+++ pages/browse.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default Browse = () => {
+    return (
+        <View>
+            <Text>Browse page</Text>
+        </View>
+    );
+}
---
+++ pages/pick.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default Pick = () => {
+    return (
+        <View>
+            <Text>Pick page</Text>
+        </View>
+    );
+}
