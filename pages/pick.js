f29ec4927f497168a9043620788adb9b3447fcb2
Cimorexave <sadeq.s502@gmail.com>
2023-09-07 12:40:34 +0330

    picking genre buttons

--- pages/pick.js
+++ pages/pick.js
@@ -1,9 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import { styles } from "../styles/app.styles";
+import { pickStyles } from "../styles/pick.styles";
+import { useState } from "react";
+import myColors from "../styles/colors.styles";
 
 export default Pick = () => {
+    const genres = ['Drama', 'Comedy', 'Romance', 'Thriller', 'Action', 'Crime', 'Hindi', 'Western', 'Horror', 'Fantasy', 'Musical', 'War', 'Noir']
+    const [isLoading, setIsLoading] = useState(false);
+    const [selectedGenres, setSelectedGenres] = useState([]);
+    const pickRandom = () => {
+        setIsLoading(!isLoading);
+    }
+    const selectGenre = (newGenre) => {
+        if (selectedGenres.includes(newGenre)) setSelectedGenres([...selectedGenres.filter(g => g !== newGenre)])
+        else setSelectedGenres([...selectedGenres, newGenre]);
+    }
     return (
-        <View>
-            <Text>Pick page</Text>
+        <View style={[styles.page, pickStyles.pickPage]}>
+            <View style={styles.horizontalCenterWrapper}>
+                <TouchableOpacity style={pickStyles.pickRandomButton} onPress={pickRandom}>
+                    <Text style={[styles.text]}>Pick Randomly</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={[styles.horizontalCenterWrapper, pickStyles.genresWrapper]}>
+                {genres.map(genre =>
+                    <TouchableOpacity key={`genre-${Math.random()}`}
+                        style={[pickStyles.pickRandomButton,
+                        selectedGenres.includes(genre) ? pickStyles.selectedGenreButton : null
+                            ,]}
+                        onPress={() => selectGenre(genre)}>
+                        <Text style={[styles.text]}>{genre}</Text>
+                    </TouchableOpacity>
+                )}
+            </View>
+
+            
+
+            {isLoading ? <View style={[styles.horizontalCenterWrapper, styles.verticalCenterWrapper, pickStyles.spinnerWrapper]}>
+                <ActivityIndicator style={pickStyles.spinner} size={"large"} color={myColors.lightgreen}></ActivityIndicator>
+            </View> : null}
+
         </View>
     );
 }
