df4c193cbaa754dcee272a9ea09bd4d9b1f521f0
Cimorexave <sadeq.s502@gmail.com>
2023-09-07 11:32:39 +0330

    modal state

--- pages/browse.js
+++ pages/browse.js
@@ -1,12 +1,13 @@
 import { View, Text, Pressable } from "react-native";
 import { styles } from "../styles/app.styles";
 import { browseStyles } from "../styles/browse.styles";
+import { useState } from "react";
 
 export default Browse = () => {
+    const [isModalVisible, setModalVisible] = useState(false);
     return (
         <View style={styles.page}>
-            <Text>Browse page</Text>
-            <Pressable>
+            <Pressable onPress={() => setModalVisible(!isModalVisible)}>
                 <Text style={[styles.text, browseStyles.addButton]}>Add +</Text>
             </Pressable>
         </View>
