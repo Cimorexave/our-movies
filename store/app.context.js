eff68bb3773a61c5f11bfa45956bce9f0041e4d3
Cimorexave <sadeq.s502@gmail.com>
2023-09-07 13:10:51 +0330

    added selected genres to the app context

--- store/app.context.js
+++ store/app.context.js
@@ -1,6 +1,7 @@
 import { createContext } from "react";
 
-export const initialAppContext = {
+const initialAppContext = {
     pageName: "pick",
+    selectedGenres: [],
 }
-export const AppContext = createContext(initialAppContext);
+export default AppContext = createContext(initialAppContext);
