e09bc47d19f6accaf74dcbe3d5e942f55676d15f
Cimorexave <sadeq.s502@gmail.com>
2023-09-07 11:05:03 +0330

    updating context state and using in menu selection

--- components/Footer.js
+++ components/Footer.js
@@ -4,23 +4,28 @@ import { useContext, useState } from "react";
 import { AppContext } from "../store/app.context";
 
 export default function Footer() {
-    const [selectedPageName, setSelectedPageName] = useState("pick");
-    const appContext = useContext(AppContext);
-    
+
+    const { appContext, setAppContext } = useContext(AppContext);
+
     return (
         <View style={styles.footer}>
             <Pressable
-                style={[styles.pressable, selectedPageName === "pick" ? styles.pressed : {}]}
-                // onPress={() => console.log('jhi')}
-                onPress={() => setSelectedPageName("pick")}
+                style={[styles.pressable,
+                appContext.pageName === "pick" ? styles.pressed : {}
+                ]}
+                // onPress={() => setSelectedPageName("pick")}
+                onPress={() => setAppContext({ pageName: "pick" })}
             >
                 <Text style={styles.text}>
                     Pick
                 </Text>
             </Pressable>
             <Pressable
-                style={[styles.pressable, selectedPageName === "browse" ? styles.pressed : {}]}
-                onPress={() => setSelectedPageName("browse")}
+                style={[styles.pressable,
+                appContext.pageName === "browse" ? styles.pressed : {}
+                ]}
+                // onPress={() => setSelectedPageName("browse")}
+                onPress={() => setAppContext({ pageName: "browse" })}
             >
                 <Text style={styles.text}>
                     Browse
